9da9e4839dbafb285ab4a4dae276dcedb600a8c1
dmcclellan3 <wdmcclellan3@gmail.com>
2024-06-18 11:54:57 -0400

    removed nested tree structure

---
+++ src/Course.jsx
@@ -0,0 +1,35 @@
+// src/components/Course/CourseList.js
+import React, { useEffect, useState } from 'react';
+import axios from 'axios';
+
+
+
+
+const CourseList = () => {
+  const [courses, setCourses] = useState([]);
+
+  useEffect(() => {
+    const fetchCourses = async () => {
+      try {
+        const response = await axios.get('/api/courses/');
+        setCourses(response.data);
+      } catch (error) {
+        console.error(error);
+      }
+    };
+    fetchCourses();
+  }, []);
+
+  return (
+    <div>
+      <h1>Courses</h1>
+      <ul>
+        {courses.map(course => (
+          <li key={course.id}>{course.name}</li>
+        ))}
+      </ul>
+    </div>
+  );
+};
+
+export default CourseList;
