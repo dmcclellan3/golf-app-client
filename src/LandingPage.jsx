6ffb8ccff8df42efc8f389de92518802ec4946d6
dmcclellan3 <wdmcclellan3@gmail.com>
2024-06-24 07:51:23 -0400

    saving

--- src/LandingPage.jsx
+++ src/LandingPage.jsx
@@ -9,7 +9,6 @@ const LandingPage = () => {
                 <h1 className="site-name">Course Caddy</h1>
                 <div className="nav-links">
                     <Link to="/login">Login</Link>
-                    <Link to="/register">Register</Link>
                 </div>
             </nav>
             <div className="hero">
